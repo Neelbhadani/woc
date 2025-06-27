99fb7a9ec950737f52597acc08e110f90b68f7d4
Neelbhadani <96937591+Neelbhadani@users.noreply.github.com>
2025-06-27 15:11:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from api import create_app
 from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Optional, if using a .env locally
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Required for Railway
+    app.run(host="0.0.0.0", port=port, debug=True)
